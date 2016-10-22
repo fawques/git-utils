1e8f805dbf79842e7458f9fdc21a1b9801b81219
Víctor Guzmán <fawques@gmail.com>
2016-10-22 13:40:54 +0200

    Use log -n 1 instead of show
    Pass all arguments to the internal log command.
    This means that the results will always come from 'git log' without arguments.
    Add TODO.

--- git_log_branch.sh
+++ git_log_branch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 log_branch(){
-	commit_list=$(git log --pretty="%H" $@)
+	commit_list=$(git log --pretty="%H")
 	for commit_hash in $commit_list;do
-		show=$(git show $commit_hash)
+		show=$(git log -n 1 $commit_hash $@)
 	 	branches=$(git branch --column --contain $commit_hash | sed -e 's/* /*/' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 	 	output=$(echo "$show" | sed "s@$commit_hash@$commit_hash {$branches}@")
 	 	echo "$output"
